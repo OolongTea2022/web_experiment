4f33a76b040398ac7ef508d78b521de40cfc6fcb
OolongTea <jiangbx0422@126.com>
2025-04-26 13:21:34 +0800

    feat:异常类

---
+++ web_experiment/src/main/java/com/wtwlc/web_experiment/exception/BusinessException.java
@@ -0,0 +1,30 @@
+package com.wtwlc.web_experiment.exception;
+
+import com.wtwlc.web_experiment.common.ErrorCode;
+import lombok.Getter;
+
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    /**
+     * 错误码
+     */
+    private final int code;
+
+    public BusinessException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+    }
+
+}
